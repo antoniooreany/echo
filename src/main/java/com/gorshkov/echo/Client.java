b99619695a48ef5c2e8562679f01e7b347ed7929
unknown <antonio_oreany@mail.ru>
2022-02-01 16:13:12 +0200

    client-server via Input/Output streams implemented, does work.

--- src/main/java/com/gorshkov/echo/Client.java
+++ src/main/java/com/gorshkov/echo/Client.java
@@ -2,7 +2,6 @@ package com.gorshkov.echo;
 
 import java.io.*;
 import java.net.Socket;
-import java.nio.charset.StandardCharsets;
 
 //client -> ${content}
 //        server -> echo ${content}
@@ -13,16 +12,17 @@ import java.nio.charset.StandardCharsets;
 
 public class Client {
 
-    private static final String content = "Hello from client";
+    private static final String CONTENT = "Hello from client";
+    private static final int BUFFER_SIZE = 512;
 
     public static void main(String[] args) throws IOException {
         try (Socket socket = new Socket("localhost", 3000);
-             OutputStream outputStream = new BufferedOutputStream(socket.getOutputStream());
              InputStream inputStream = new BufferedInputStream(socket.getInputStream());
+             OutputStream outputStream = new BufferedOutputStream(socket.getOutputStream());
         ) {
-            outputStream.write(content.getBytes());
-
-            byte[] buffer = new byte[20];
+            outputStream.write(CONTENT.getBytes());
+            outputStream.flush();
+            byte[] buffer = new byte[BUFFER_SIZE];
             int count = inputStream.read(buffer);
 
             String contentFromServer = new String(buffer, 0, count);
--- src/main/java/com/gorshkov/echo/Server.java
+++ src/main/java/com/gorshkov/echo/Server.java
@@ -3,6 +3,7 @@ package com.gorshkov.echo;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 
 //client -> ${content}
 //        server -> echo ${content}
@@ -12,20 +13,25 @@ import java.net.Socket;
 //        2. Reader/Writer
 
 public class Server {
+
+    private static final int BUFFER_SIZE = 512;
+
     public static void main(String[] args) throws IOException {
         try (ServerSocket serverSocket = new ServerSocket(3000);
-        ) {//listen
+        ) { //listen
             while (true) {
                 try (Socket socket = serverSocket.accept();
                      InputStream inputStream = new BufferedInputStream(socket.getInputStream());
                      OutputStream outputStream = new BufferedOutputStream(socket.getOutputStream());) {
-                    byte[] buffer = new byte[20];
+                    byte[] buffer = new byte[BUFFER_SIZE];
                     int count = inputStream.read(buffer);
                     String content = new String(buffer, 0, count);
                     System.out.println(content);
-
                     String message = "echo " + content;
                     outputStream.write(message.getBytes());
+//                    count = inputStream.read(buffer);
+//                    content = new String(buffer, 0, count);
+//                    System.out.println(content);
                 }
             }
         }
